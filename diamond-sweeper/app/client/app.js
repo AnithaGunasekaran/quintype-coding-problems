4af77a60c8bcd2d97b4a2b497d18836ba010f881
AnithaGunasekaran <AnithaGunasekaran@users.noreply.github.com>
2018-09-06 07:07:44 +0530

    Initial Commit. Project setup.

--- diamond-sweeper/app/client/app.js
+++ diamond-sweeper/app/client/app.js
@@ -1,3 +1,30 @@
 global.startApp = function(container) {
   console.log("Here is the container:", container);
-}
+
+  $(document).ready(function() {
+    //Reset all squares to have "?" marks
+    resetSquares();
+    var dSquares = genDiamondSquare();
+    console.log(dSquares);
+  });
+
+  $(".diamondsweeper-board").delegate("td", "click", function() {});
+
+  function resetSquares() {
+    $(".diamondsweeper-board td div")
+      .addClass("unknown")
+      .removeClass("arrow diamond");
+  }
+
+  function genDiamondSquare() {
+    var dArr = [];
+    for (var i = 0; i < 7; i++) {
+      var rand = Math.round(Math.random() * 63);
+      if (!$.inArray(rand, dArr)) {
+        dArr.push(rand);
+      }
+    }
+    return dArr;
+  }
+  function bindEvent() {}
+};
--- diamond-sweeper/start.js
+++ diamond-sweeper/start.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
+
+const compiler = webpack(webpackConfig);
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-  res.render("home/index")
+app.get("/", function(req, res) {
+  res.render("home/index");
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3004, function() {
+  console.log("Example app listening on port 3004!");
 });
